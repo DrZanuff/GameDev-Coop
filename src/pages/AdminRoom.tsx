d0e41f6b6bb12da49789e5d6a2174ed1b5e72e2d
Ricardo <ricardo.machado.nwi@gmail.com>
2021-06-26 21:41:27 -0300

    Added image to Docs

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -69,7 +69,7 @@ export function AdminRoom(){
             <main>
                 <div className='room-title'>
                     <h1>Sala {title}</h1>
-                    { questions.length > 0 && <span>{questions.length} pergunta(s)</span> }
+                    { questions.length > 0 && <span>{questions.length} sugestion(s)</span> }
                 </div>
 
                 <div className='lists'>
